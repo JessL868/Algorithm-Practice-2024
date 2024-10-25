a965617688d30b61f14b9f2a81b40748434a8ab0
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-10-25 18:02:21 -0400

    practice

--- practice.js
+++ practice.js
@@ -11,16 +11,16 @@
 // };
 // console.log(factorial(10))
 
-function parkingSpaces(n, yesterday, today) {
-    let count = 0;
-    for(let i = 0; i < n; i++){
-        if(yesterday[i] === 'C' && today[i] === 'C') {
-            count++;
-        }
-    }
-    return count;
-}
-console.log(checkSpaces(5, "CC..C", ".CC.."));
+// function parkingSpaces(n, yesterday, today) {
+//     let count = 0;
+//     for(let i = 0; i < n; i++){
+//         if(yesterday[i] === 'C' && today[i] === 'C') {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(checkSpaces(5, "CC..C", ".CC.."));
 //algorithm
 
 // input binary
@@ -29,11 +29,29 @@ console.log(checkSpaces(5, "CC..C", ".CC.."));
 //  wholeNumber = eachNumber += eachNumber
 // console.log(wholeNumber)
 
-function binToDec(x) {
-    for (i = 0; i < x; i--) {
-        eachNumber = i*Math.pow(2, i-1);
-        wholeNumber = eachNumber += eachNumber
-    };
-    console.log(wholeNumber);
-};
-binToDec(1101)
+// function binToDec(x) {
+//     let decimal = 0;
+//     for (let i = 0; i < x.length; i++) {
+//         if(binary[binary.length-1-1] === "1") {
+//             decimal = decimal + Math.pow(2, i);
+//         }
+//     }
+//     return decimal;
+// };
+// console.log(binToDec("1101"));
+
+function decToBin(x) {
+    let bin = "";
+    if (x === 0) {
+        return 0;
+    }
+    while(x >= 1) {
+        let remainder = x % 2;
+        bin = remainder + bin;
+        decimal = Math.floor(x / 2);
+    }
+    return binary;
+}
+console.log(decToBin(9));
+//divide the nuimber by the largest exponent of 2
+// if u can still divide it by 2 you would 
